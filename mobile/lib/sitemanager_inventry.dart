fdd1ce08098ed664880a4e5d6ad2e87b68decb28
Viraj <avjoachim@hotmail.com>
2021-09-30 21:37:26 +0530

    forms commit

---
+++ mobile/lib/sitemanager_inventry.dart
@@ -0,0 +1,12 @@
+// ignore: unused_import
+
+class User {
+  String item_name;
+  String item_description;
+  String site_name;
+  int item_capacity;
+  int available_quantity;
+  String measuring_unit;
+  int unit_price;
+  User( this.item_name, this.item_description, this.site_name, this.item_capacity, this.available_quantity, this.measuring_unit, this.unit_price);
+}
